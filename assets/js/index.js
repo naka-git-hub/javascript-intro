e6f7a53fb21c943808390ec4bc22c44bade2bb76
naka-git-hub <naka.rafilipomanana@gmail.com>
2025-07-18 15:53:00 +0200

    first-update

---
+++ assets/js/index.js
@@ -0,0 +1,63 @@
+// let text = "javascript file";
+
+// alert('hello work from js file : ' + text);
+
+// let price1 = 10; //integer
+// let price2 = 20; //integer
+// let price3 = " aujourd'hui"; //string
+// let price4;
+// let total = 0;
+
+// total = price1 + price2;
+
+// console.log("Prix addition : " + total);
+
+// total = 0;
+
+// total = price1 - price2;
+
+// console.log("Prix soustraction : " + total);
+
+// total = 0;
+
+// total = total + price3;
+
+// console.log("Prix total : " + total);
+
+// total = 0;
+
+// total = total + price4;
+
+// console.log("Prix total : " + total);
+
+
+// exo 1 :
+const nom = "Naka";
+console.log("Nom : " + nom);
+alert("Nom : " + nom);
+
+// exo 2 :
+let age = 39;
+let message = "Je suis " + nom + " et j'ai " + age; 
+console.log(message);
+
+// exo 3 :
+const ageOneMore = age + 1;
+message = "l'année prochaine j'aurai " + ageOneMore;
+console.log(message);
+
+// exo 4 :
+let toto = 'toto';
+let tata = 'tata';
+
+// console.log("Avant inversement, toto = " + toto);
+// console.log("Avant inversement, tata = " + tata);
+
+let temp = tata;
+tata = toto;
+toto = temp;
+
+// console.log("Après inversement, toto = " + toto);
+// console.log("Après inversement, tata = " + tata);
+
+console.log("dans toto : " + toto, "dans tata : ", tata)
